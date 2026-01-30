942a8e169ad3ca56b4d0e349fc3079c1e0eb59b5
solaiman <solaiman6551@gmail.com>
2026-01-31 00:56:35 +0600

    -color utils error fixed

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -4,11 +4,16 @@ Color primaryColor = const Color(0xFF4A90E2);
 Color bgPrimaryColor = const Color(0xFFF5F5F7);
 Color shimmerBaseColor = const Color(0xFFE8EBF2);
 Color shimmerBaseHighlightColor = const Color(0xFFF4F6F9);
+
 Color primaryWhite = const Color(0xFFFFFFFF);
+Color secondaryColor = const Color(0xFFE62E04);
+Color primaryRedColor = const Color(0xFFE62E04);
 Color suvaGreyColor = const Color(0xFFEEEEEE);
+Color commonBorderColor = const Color(0xFFBFBFBF);
 Color lightGreyColor = const Color(0xFF838383);
 Color primaryTextColor = const Color(0xFF131C31);
 Color secondaryTextColor = const Color(0xFF2f2f2f);
 Color primaryTextLightColor = const Color(0xFF6B6B6B);
 Color transparentColor = const Color(0x00000000);
+Color black54 = const Color(0xFF545454);
 
