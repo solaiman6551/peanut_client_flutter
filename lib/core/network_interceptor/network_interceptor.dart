135de4222b8f703a576a10baaf52c91cea20e9a6
solaiman <solaiman6551@gmail.com>
2026-01-31 01:11:50 +0600

    -internet connectivity check added

--- lib/core/network_interceptor/network_interceptor.dart
+++ lib/core/network_interceptor/network_interceptor.dart
@@ -1,6 +1,9 @@
 import 'dart:developer';
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:dio/dio.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../../common/toasts.dart';
 
 
 class NetworkInterceptor extends InterceptorsWrapper {
@@ -10,13 +13,16 @@ class NetworkInterceptor extends InterceptorsWrapper {
   NetworkInterceptor(this.ref);
 
   @override
-  void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
-
-    options.headers['Content-Type'] = 'application/json';
-    options.headers['Cache-Control'] = 'no-cache';
-    options.headers['requestTime'] = '';
-    return super.onRequest(options, handler);
-
+  void onRequest(RequestOptions options, RequestInterceptorHandler handler) async {
+    final List<ConnectivityResult> connectivityResult = await Connectivity().checkConnectivity();
+    if (connectivityResult.contains(ConnectivityResult.wifi)) {
+      return super.onRequest(options, handler);
+    } else if (connectivityResult.contains(ConnectivityResult.mobile)) {
+      return super.onRequest(options, handler);
+    } else if (connectivityResult.contains(ConnectivityResult.none)) {
+      EasyLoading.dismiss();
+      Toasts.showErrorToast("Please check you internet connection");
+    }
   }
 
   @override
